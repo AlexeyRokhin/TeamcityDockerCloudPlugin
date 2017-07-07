5101758715c9d9c25ee818d9676b54fe558568d4
Alexey Rokhin <arokhin@mail.ru>
2017-07-07 12:29:27 +0300

    Implement storage options settings
    Signed-off-by: Alexey Rokhin <arokhin@mail.ru>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.bundling.Zip
 
-version = "0.4.1"
+version = "0.4.1-storage-opt"
 group = "var.run.docker.cloud"
 val commitId = gitCommitId()
 allprojects {
